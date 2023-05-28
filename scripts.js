b5b33637d53d2ea801b7910faaa94e65cb6c88e4
takumiengineer <101326931+takumi212@users.noreply.github.com>
2023-05-28 16:37:48 +0900

    Add files via upload
    I insert a number of contents.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,6 @@
 
 
 
-// $(document).ready(function() {
-//   $('header').click(function() {
-//     $('.outline1').fadeOut(1000, function() {
-//       $('.outline2').fadeIn(500);
-//       $('.outline2').html('<img src="blue_rose.png" alt="Blue Rose" width="200">');
-//     });
-//   });
-// });
-
-
 $(document).ready(function() {
   $('header').click(function() {
     $('.outline1').fadeOut(1000, function() {
@@ -21,24 +11,6 @@ $(document).ready(function() {
 });
 
 
-
-
-
-
-  
-// 背景に泡が流れているようにしている。
-  
-// bubbly({
-//   gradientStart: '#fff4e6',
-//   gradientStop: '#ffe9e4',
-//   shadowBlur: 1,
-//   compose: 'source-over',
-//   fillFunc: () => `hsla(${Math.random() * 50}, 100%, 50%, .3)`,
-// });
-    
-  
-  // 僕の写真が出現する
-  
   
   
   
