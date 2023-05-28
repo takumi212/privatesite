e68952416b209afe377666a4dfa72fc6f08a561a
takumiengineer <101326931+takumi212@users.noreply.github.com>
2023-05-28 16:00:14 +0900

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,50 @@
+
+
+
+// $(document).ready(function() {
+//   $('header').click(function() {
+//     $('.outline1').fadeOut(1000, function() {
+//       $('.outline2').fadeIn(500);
+//       $('.outline2').html('<img src="blue_rose.png" alt="Blue Rose" width="200">');
+//     });
+//   });
+// });
+
+
+$(document).ready(function() {
+  $('header').click(function() {
+    $('.outline1').fadeOut(1000, function() {
+      $('.outline2').fadeIn(500);
+      $('.outline2').addClass('glowing-text');
+    });
+  });
+});
+
+
+
+
+
+
+  
+// 背景に泡が流れているようにしている。
+  
+// bubbly({
+//   gradientStart: '#fff4e6',
+//   gradientStop: '#ffe9e4',
+//   shadowBlur: 1,
+//   compose: 'source-over',
+//   fillFunc: () => `hsla(${Math.random() * 50}, 100%, 50%, .3)`,
+// });
+    
+  
+  // 僕の写真が出現する
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
